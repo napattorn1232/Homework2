20fc49863298a90c18082cc4859e2419debac0a0
napattorn1232 <67993385+napattorn1232@users.noreply.github.com>
2020-07-15 21:05:27 +0700

    Add files via upload

---
+++ homework2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void main()
+{
+	string fname, lname;
+	int salary , sale , commision , total ;
+
+	cout << "***Homework2***" << endl;
+
+	cout << "Enter Name : "; 
+	cin >> fname >> lname ;
+	
+	cout << "Enter Salary : ";
+	cin >> salary ;
+	
+	cout << "Enter sale : " ;
+	cin >> sale ;
+	
+	cout << "Enter commision : " ;
+	cin >> commision ;
+
+	cout << "- - - - -output- - - - - - -" << endl;
+	
+	cout << " Your name = " << fname << " " << lname << endl;
+
+	total = (sale*commision/100)+salary ;
+	cout << " Total Revenue " << total << " Bath" << endl;
+	cout << "------------------------------------------" << endl;
+
+
+}
